d369de8eaa18f1e04e2d0d3bd920c712fcb8a046
paulaan <me@paulaan.me>
2024-03-05 22:36:06 +0700

    push llm conflict

--- metagpt/llm.py
+++ metagpt/llm.py
@@ -5,27 +5,16 @@
 @Author  : alexanderwu
 @File    : llm.py
 """
-
 from typing import Optional
 
-from metagpt.config import LLMProviderEnum, CONFIG
+from metagpt.configs.llm_config import LLMConfig
+from metagpt.context import Context
 from metagpt.provider.base_llm import BaseLLM
-from metagpt.provider.human_provider import HumanProvider
-from metagpt.provider.llm_provider_registry import LLM_REGISTRY
-
-_ = HumanProvider()  # Avoid pre-commit error
 
 
-def LLM(provider: Optional[LLMProviderEnum] = None) -> BaseLLM:
-    if CONFIG.gemini_api_key:
-        provider = LLMProviderEnum.GEMINI
-    if provider is None and CONFIG.openai_api_key:
-        provider = LLMProviderEnum.OPENAI
-    else:
-        raise RuntimeError("You should config a LLM configuration first")
-
-    """get the default llm provider"""
-    if provider is None:
-        provider = CONFIG.get_default_llm_provider_enum()
-
-    return LLM_REGISTRY.get_provider(provider)
+def LLM(llm_config: Optional[LLMConfig] = None, context: Context = None) -> BaseLLM:
+    """get the default llm provider if name is None"""
+    ctx = context or Context()
+    if llm_config is not None:
+        return ctx.llm_with_cost_manager_from_llm_config(llm_config)
+    return ctx.llm()
